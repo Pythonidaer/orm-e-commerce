166bb631c3a55f3ba05093176880324fc12fdf52
Pythonidaer <pythonidaer@gmail.com>
2021-03-16 21:20:57 -0400

    Setup my homework repository. There is a config, db, models, routes, and seeds folder. There are also most of the npm packages installed. The .env file is setup and excloded from the file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+/*
+Do I have to import a model here to sync table with database? See 03-Ins_Models
+*/
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
+
+
+/* Homework supplied script:
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+*/
